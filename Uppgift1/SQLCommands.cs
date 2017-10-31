355cc2fb2267b2555f1f8b0a149cc0e21b27744f
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2017-10-31 14:47:56 +0100

    städa kod

--- Uppgift1/SQLCommands.cs
+++ Uppgift1/SQLCommands.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Uppgift1.DAL;
 
 namespace Uppgift1
