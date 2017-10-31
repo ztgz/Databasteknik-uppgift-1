8f1dd23141a01ba6ea314188ce5b9c54027f4261
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2017-10-31 14:42:44 +0100

    tagit bort unödiga refernser

--- Uppgift1/UI/MoreInfoWindow.cs
+++ Uppgift1/UI/MoreInfoWindow.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Uppgift1.DAL;
+﻿using System.Windows.Forms;
 
 namespace Uppgift1.UI
 {
